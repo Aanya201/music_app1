4935de3448a976a711008564d5e95fe99fa6ba5c
Aanya201 <115004980+Aanya201@users.noreply.github.com>
2022-12-02 19:55:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_1 ="";
 song_2 ="";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
 
 function setup(){
     canvas= createCanvas(600, 500);
@@ -7,6 +11,13 @@ function setup(){
  
      video = createCapture(VIDEO);
      video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
+ }
+
+ function modelLoaded() {
+    console.log('PoseNet Is Initiated');
  }
 
  function preload() {
@@ -16,4 +27,16 @@ function setup(){
 
  function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        
+    }
 }
